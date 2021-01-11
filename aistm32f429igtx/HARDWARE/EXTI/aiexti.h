74ae981b7cc30150a0f4532f6faa277a20640124
takeno2020 <takeno2020@163.com>
2021-01-11 22:51:46 +0800

    STM32F429IGTx:提交串口和中断实验程序.

---
+++ aistm32f429igtx/HARDWARE/EXTI/aiexti.h
@@ -0,0 +1,14 @@
+#ifndef AI_EXTI_H_
+#define AI_EXTI_H_	
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+    
+void aiextix_init(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* AI_EXTI_H_ */
--- aistm32f429igtx/USER/main.c
+++ aistm32f429igtx/USER/main.c
@@ -8,6 +8,8 @@
 #include <aidelay.h>
 #include <ailed.h>
 #include <aikey.h>
+#include <aiusart.h>
+#include "aiexti.h"
 
 /*
 ********************************************************************************
@@ -23,37 +25,41 @@
 */
 int main(void)
 {
-    u8 key;
+	u8 i;
+	u8 len = 0;
+	u16 times = 0;
     
     ai_sys_clock_init(360, 25, 2, 8);    // 设置时钟180MHz
     ai_delay_init(180);
+	ai_uart_init(90, 115200);
     ai_led_init();
     ai_key_init();
+    aiextix_init();
     
     ai_led_on(AI_LED_DS0);
     ai_led_off(AI_LED_DS1);
     
     while (1) {
-        key = ai_key_scan(0);
-        if (key != AI_KEY_ALL_UP) {
-            switch (key) {
-            case AI_KEY_WK_DOWN:
-                AI_DS0 = !AI_DS0;
-                AI_DS1 = !AI_DS0;
-                break;
-            case AI_KEY0_DOWN:
-                AI_DS0 = !AI_DS0;
-                break;
-            case AI_KEY1_DOWN:
-                AI_DS1 = !AI_DS1;
-                break;
-            case AI_KEY2_DOWN:
-                AI_DS0 = !AI_DS0;
-                AI_DS1 = !AI_DS1;
-                break;
-            }
-        } else {
-            ai_delay_ms(10);
-        }
+		if (AI_USART_RX_STA & 0x8000) {
+			len = AI_USART_RX_STA & 0x3fff;
+			printf("\r\n您发送的消息为：\r\n");
+			for (i = 0; i < len; i++) {
+				USART1->DR = AI_USART_RX_BUF[i];
+				while ((USART1->SR & 0x40) == 0)
+					;
+			}
+			printf("\r\n\r\n");
+			AI_USART_RX_STA = 0;
+		} else {
+			times++;
+			if (times % 5000 == 0) {
+				printf("\r\nSTM32F429IGTx开发板 - 串口实验\r\n");
+				printf("takeno\r\n\r\n\r\n");
+			}
+			
+			if (times % 200 == 0)
+				printf("请输入数据，以回车键结束\r\n");
+			ai_delay_ms(10);
+		}
 	}
 }
